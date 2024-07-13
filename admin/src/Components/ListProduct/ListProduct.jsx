d5eccba158baa90c27a5ddc4559878e9fd3a464f
Nijalinga <156662938+Nijalinga@users.noreply.github.com>
2024-07-13 23:03:01 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,70 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import './ListProduct.css'
-// import cross_icon from '../../assets/cross_icon.png'
-
-
-// const ListProduct = () => {
-
-//  const [allproducts,setAllProducts] =useState([]);
-
-//  const fetchInfo = async()=>{
-//    await fetch ('http://localhost:4000/allproducts')
-//    .then((res)=>res.json())
-//    .then((data)=>{setAllProducts(data)});
-   
-//  }
-
-//  useEffect(()=>{
-//      fetchInfo();
-//  },[])
-
-//   const remove_product = async (id)=>{
-//      await fetch('http://localohost:4000/removeproduct',{
-//         method:'POST',
-//         headers:{
-//           Accept:'application/json',
-//           'Content-Type':'application/json',
-//         },
-//         body:JSON.stringify({id:id})
-//       })
-//       await  fetchInfo();
-//   }
-
-//   return (
-//     <div className='list-product'>
-//       <h1>All Products List</h1>
-//       <div className="listproduct-format-main">
-//         <p>Products</p>
-//         <p>Title</p>
-//         <p>Old Price</p>
-//         <p>New Price</p>
-//         <p>Category</p>
-//         <p>Remove</p>
-//       </div>
-//       <div className="listproduct-allproducts">
-//         <hr />
-
-//         {allproducts.map((product,index)=>{
-//             return<>
-//             <div key={index}className="listproduct-format-main listproduct-format"> 
-//              <img src={product.image} alt="" className="listproduct-product-icon" />
-//              <p>{product.name}</p>
-//              <p>${product.old_price}</p>
-//              <p>${product.new_price}</p>
-//              <p>{product.category}</p>
-//              <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon'  src={cross_icon} alt=""/>
-//             </div>
-//             <hr />
-//             </> 
-//         })}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default ListProduct
-
-
 import React, { useEffect, useState } from 'react';
 import './ListProduct.css';
 import cross_icon from '../../assets/cross_icon.png';
@@ -74,7 +7,7 @@ const ListProduct = () => {
 
   const fetchInfo = async () => {
     try {
-      const response = await fetch('https://shopper-niju-backend.onrender.com');
+      const response = await fetch('https://shopper-niju-backend.onrender.com/allproducts');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -91,7 +24,7 @@ const ListProduct = () => {
 
   const removeProduct = async (id) => {
     try {
-      const response = await fetch('https://shopper-niju-backend.onrender.com', {
+      const response = await fetch('https://shopper-niju-backend.onrender.com/removeproduct', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
