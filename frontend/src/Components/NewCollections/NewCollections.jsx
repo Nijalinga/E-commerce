23aa4f5e4e428093059364de1d62bfde735339a5
Nijalinga <156662938+Nijalinga@users.noreply.github.com>
2024-07-13 23:56:26 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection]=useState([]);
 
   useEffect(()=>{
-     fetch('https://shopper-niju-backend.onrender.com/newcollections')
+     fetch('e-backend-shoppify-niju.vercel.app/newcollections')
      .then((response)=>response.json())
      .then((data)=>setNew_collection(data));
   },[])
