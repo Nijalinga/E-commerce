ea43ff4fd948a68201fd6c32654eab889b8064a5
Nijalinga <156662938+Nijalinga@users.noreply.github.com>
2024-07-14 22:31:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const cors=require("cors");
 app.use(express.json());
 app.use(cors(
       {
-        origin:"",
+        origin:"https://shoppify-nijuu-backend.vercel.app",
         methods:['GET','POST'],
         credentials:true,
     }
